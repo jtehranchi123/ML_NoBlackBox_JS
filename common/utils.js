f77590b1e14d1e7b111ebf93f2c92a07bb7cfa9a
Jordan Tehranchi <jordan.tehranchi@yahoo.com>
2023-04-18 22:22:02 -0700

    Added in feature selection and chart dynamics for being able to view all drawings eloquently and see Point Count and Path Count for the drawings in a nice chart GUI

--- common/utils.js
+++ common/utils.js
@@ -1,19 +1,42 @@
 const utils = {};
 
-utils.formatPercent=(n)=>{
-    return (n*100).toFixed(2) + "%";
-}
+utils.flaggedUsers = [1663882102141, 1663900040545, 1664485938220];
 
-utils.printProgress=(count,max)=>{
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
-    const percent = utils.formatPercent(count/max);
+utils.styles = {
+  car: { color: "gray", text: "🚗" },
+  fish: { color: "red", text: "🐠" },
+  house: { color: "yellow", text: "🏠" },
+  tree: { color: "green", text: "🌳" },
+  bicycle: { color: "cyan", text: "🚲" },
+  guitar: { color: "blue", text: "🎸" },
+  pencil: { color: "magenta", text: "✏️" },
+  clock: { color: "lightgray", text: "🕒" },
+};
 
-    process.stdout.write(count+"/" + max + " (" + percent + ")");
+utils.formatPercent = (n) => {
+  return (n * 100).toFixed(2) + "%";
+};
 
-}
+utils.printProgress = (count, max) => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  const percent = utils.formatPercent(count / max);
 
+  process.stdout.write(count + "/" + max + " (" + percent + ")");
+};
 
-if (typeof module !== 'undefined'){
-    module.exports = utils;
+utils.groupBy = (objArray, key) => {
+  const groups = {};
+  for (let obj of objArray) {
+    const val = obj[key];
+    if (groups[val] == null) {
+      groups[val] = [];
+    }
+    groups[val].push(obj);
+  }
+  return groups;
+};
+
+if (typeof module !== "undefined") {
+  module.exports = utils;
 }
